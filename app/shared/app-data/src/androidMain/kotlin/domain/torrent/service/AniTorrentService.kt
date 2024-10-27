5a61668f62ad10c582108c53f4d656c8e2cfc50f
StageGuard <sg@mamoe.net>
2024-10-27 15:36:15 +0800

    optimize notification

--- app/shared/app-data/src/androidMain/kotlin/domain/torrent/service/AniTorrentService.kt
+++ app/shared/app-data/src/androidMain/kotlin/domain/torrent/service/AniTorrentService.kt
@@ -9,14 +9,7 @@
 
 package me.him188.ani.app.domain.torrent.service
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Intent
-import android.graphics.Bitmap
-import android.graphics.drawable.Icon
-import android.os.Build
 import android.os.IBinder
 import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.lifecycleScope
@@ -25,24 +18,16 @@ import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import kotlinx.io.files.Path
-import kotlinx.serialization.json.Json
 import me.him188.ani.app.data.models.preference.AnitorrentConfig
 import me.him188.ani.app.data.models.preference.ProxySettings
 import me.him188.ani.app.data.models.preference.TorrentPeerConfig
-import me.him188.ani.app.domain.torrent.IAnitorrentConfigCallback
-import me.him188.ani.app.domain.torrent.IProxySettingsCallback
-import me.him188.ani.app.domain.torrent.IRemoteAniTorrentEngine
-import me.him188.ani.app.domain.torrent.IRemoteTorrentDownloader
-import me.him188.ani.app.domain.torrent.ITorrentPeerConfigCallback
 import me.him188.ani.app.domain.torrent.engines.AnitorrentEngine
-import me.him188.ani.app.domain.torrent.parcel.PAnitorrentConfig
-import me.him188.ani.app.domain.torrent.parcel.PProxySettings
-import me.him188.ani.app.domain.torrent.parcel.PTorrentPeerConfig
-import me.him188.ani.app.domain.torrent.service.proxy.TorrentDownloaderProxy
+import me.him188.ani.app.domain.torrent.service.proxy.TorrentEngineProxy
 import me.him188.ani.app.torrent.anitorrent.AnitorrentDownloaderFactory
+import me.him188.ani.datasources.api.topic.FileSize.Companion.bytes
 import me.him188.ani.utils.io.inSystem
 import me.him188.ani.utils.logging.info
 import me.him188.ani.utils.logging.logger
@@ -59,58 +44,20 @@ class AniTorrentService : LifecycleService(), CoroutineScope {
     private val torrentPeerConfig: MutableSharedFlow<TorrentPeerConfig> = MutableSharedFlow(1)
     private val anitorrentConfig: MutableSharedFlow<AnitorrentConfig> = MutableSharedFlow(1)
     
-    private val json = Json { 
-        ignoreUnknownKeys = true
-        encodeDefaults = true
-    }
-    
     private val anitorrent: CompletableDeferred<AnitorrentEngine> = CompletableDeferred()
 
-    private val binder = object : IRemoteAniTorrentEngine.Stub() {
-        override fun getAnitorrentConfigFlow(): IAnitorrentConfigCallback {
-            return object : IAnitorrentConfigCallback.Stub() {
-                override fun onEmit(config: PAnitorrentConfig?) {
-                    logger.info { "received client AnitorrentConfig: $config" }
-                    if (config != null) anitorrentConfig.tryEmit(
-                        json.decodeFromString(AnitorrentConfig.serializer(), config.serializedJson)
-                    )
-                }
-            }
-        }
-
-        override fun getProxySettingsFlow(): IProxySettingsCallback {
-            return object : IProxySettingsCallback.Stub() {
-                override fun onEmit(config: PProxySettings?) {
-                    logger.info { "received client ProxySettings: $config" }
-                    if (config != null) proxySettings.tryEmit(
-                        json.decodeFromString(ProxySettings.serializer(), config.serializedJson)
-                    )
-                }
-            }
-        }
-
-        override fun getTorrentPeerConfigFlow(): ITorrentPeerConfigCallback {
-            return object : ITorrentPeerConfigCallback.Stub() {
-                override fun onEmit(config: PTorrentPeerConfig?) {
-                    logger.info { "received client TorrentPeerConfig: $config" }
-                    if (config != null) torrentPeerConfig.tryEmit(
-                        json.decodeFromString(TorrentPeerConfig.serializer(), config.serializedJson)
-                    )
-                }
-            }
-        }
-
-        override fun setSaveDir(saveDir: String?) {
-            logger.info { "received client saveDir: $saveDir" }
-            if (saveDir != null) saveDirDeferred.complete(saveDir)
-        }
-
-        override fun getDownlaoder(): IRemoteTorrentDownloader {
-            val downloader = runBlocking { anitorrent.await().getDownloader() }
-            return TorrentDownloaderProxy(downloader, coroutineContext)
-        }
-
+    private val binder by lazy {
+        TorrentEngineProxy(
+            saveDirDeferred,
+            proxySettings,
+            torrentPeerConfig,
+            anitorrentConfig,
+            anitorrent,
+            coroutineContext,
+        )
     }
+
+    private val notification = ServiceNotification(this)
     
     override fun onCreate() {
         super.onCreate()
@@ -129,6 +76,15 @@ class AniTorrentService : LifecycleService(), CoroutineScope {
             )
             logger.info { "anitorrent is initialized." }
         }
+
+        launch {
+            val anitorrentDownloader = anitorrent.await().getDownloader()
+            anitorrentDownloader.totalStats.debounce(1000).collect { stat ->
+                notification.updateNotification(
+                    NotificationDisplayStrategy.Idle(stat.downloadSpeed.bytes, stat.uploadSpeed.bytes),
+                )
+            }
+        }
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -137,7 +93,8 @@ class AniTorrentService : LifecycleService(), CoroutineScope {
             return super.onStartCommand(intent, flags, startId)
         }
 
-        pushNotification(intent)
+        notification.parseNotificationStrategyFromIntent(intent)
+        notification.createNotification(this)
         return START_STICKY
     }
     
@@ -152,63 +109,5 @@ class AniTorrentService : LifecycleService(), CoroutineScope {
         super.onUnbind(intent)
         logger.info { "client unbind anitorrent." }
         return true
-    }
-
-    private fun pushNotification(startIntent: Intent?) {
-        val notificationName = (startIntent?.getIntExtra("app_name", -1) ?: -1)
-            .let { if (it == -1) "Animeko" else getString(it) }
-        val notificationContentText = (startIntent?.getIntExtra("app_service_content_text", -1) ?: -1)
-            .let { if (it == -1) "Animeko BT 引擎正在运行中" else getString(it) }
-        val notificationStickerContent = (startIntent?.getIntExtra("app_service_content_text", -1) ?: -1)
-            .let { if (it == -1) "Animeko BT 引擎正在运行中" else getString(it) }
-        val notificationIcon = (startIntent?.getIntExtra("app_icon", -1) ?: -1)
-            .let { if (it != -1) defaultNotificationIcon else Icon.createWithResource(this, it) }
-
-        val openActivityIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            startIntent?.getParcelableExtra("open_activity_intent", Intent::class.java)
-        } else {
-            startIntent?.getParcelableExtra<Intent>("open_activity_intent")
-        }
-        
-        
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        val existingNotification = notificationManager.activeNotifications.find { it.id == NOTIFICATION_ID }
-
-        if (existingNotification == null) {
-            val channel = notificationManager.getNotificationChannel(NOTIFICATION_CHANNEL_ID)
-                ?: NotificationChannel(NOTIFICATION_CHANNEL_ID, notificationName, NotificationManager.IMPORTANCE_HIGH)
-                    .apply { lockscreenVisibility = Notification.VISIBILITY_PUBLIC }
-                    .also { notificationManager.createNotificationChannel(it) }
-
-            val openActivityAction = if (openActivityIntent == null) null else
-                PendingIntent.getActivity(this, 0, openActivityIntent, PendingIntent.FLAG_IMMUTABLE)
-            val stopServiceAction = PendingIntent.getService(
-                this, 0,
-                Intent(this, this::class.java).apply { putExtra("stopService", true) },
-                PendingIntent.FLAG_IMMUTABLE,
-            )
-
-            val notification = Notification.Builder(this, channel.id).apply {
-                setContentTitle(notificationName)
-                setContentText(notificationContentText)
-                setSmallIcon(notificationIcon)
-                setContentIntent(openActivityAction)
-                setActions(
-                    Notification.Action.Builder(notificationIcon, "停止", stopServiceAction).build(),
-                )
-                setTicker(notificationStickerContent)
-                setVisibility(Notification.VISIBILITY_PUBLIC)
-            }.build()
-
-            startForeground(NOTIFICATION_ID, notification)
-        }
-    }
-    
-    companion object {
-        private const val NOTIFICATION_ID = 114
-        private const val NOTIFICATION_CHANNEL_ID = "me.him188.ani.app.domain.torrent.service.AniTorrentService"
-        private val defaultNotificationIcon: Icon by lazy {
-            Icon.createWithBitmap(Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888))
-        }
     }
 }
---
+++ app/shared/app-data/src/androidMain/kotlin/domain/torrent/service/ServiceNotification.kt
@@ -0,0 +1,187 @@
+/*
+ * Copyright (C) 2024 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
+package me.him188.ani.app.domain.torrent.service
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.Service
+import android.app.Service.NOTIFICATION_SERVICE
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.drawable.Icon
+import android.os.Build
+import me.him188.ani.datasources.api.topic.FileSize
+import me.him188.ani.datasources.api.topic.FileSize.Companion.bytes
+
+class ServiceNotification(
+    private val context: Context
+) {
+    private val notificationService by lazy { context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager }
+
+    private var notificationAppearance = defaultNotificationAppearance
+    private var notificationOpenActivityIntent: Intent? = null
+    private val stopServiceIntent by lazy {
+        PendingIntent.getService(
+            context, 0,
+            Intent(context, AniTorrentService::class.java).apply { putExtra("stopService", true) },
+            PendingIntent.FLAG_IMMUTABLE,
+        )
+    }
+
+    private val notificationChannel by lazy {
+        notificationService.getNotificationChannel(NOTIFICATION_CHANNEL_ID)
+            ?: NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                notificationAppearance.name,
+                NotificationManager.IMPORTANCE_HIGH,
+            )
+                .apply { lockscreenVisibility = Notification.VISIBILITY_PUBLIC }
+                .also { notificationService.createNotificationChannel(it) }
+    }
+
+    fun parseNotificationStrategyFromIntent(intent: Intent?): NotificationAppearance {
+        val name = (intent?.getIntExtra("app_name", -1) ?: -1)
+            .let { if (it == -1) defaultNotificationAppearance.name else context.getString(it) }
+        val titleIdle = (intent?.getIntExtra("app_service_title_text_idle", -1) ?: -1)
+            .let { if (it == -1) defaultNotificationAppearance.titleIdle else context.getString(it) }
+        val titleWorking = (intent?.getIntExtra("app_service_title_text_working", -1) ?: -1)
+            .let { if (it == -1) defaultNotificationAppearance.titleWorking else context.getString(it) }
+        val content = (intent?.getIntExtra("app_service_content_text", -1) ?: -1)
+            .let { if (it == -1) defaultNotificationAppearance.content else context.getString(it) }
+        val stopActionText = (intent?.getIntExtra("app_service_stop_text", -1) ?: -1)
+            .let { if (it == -1) defaultNotificationAppearance.name else context.getString(it) }
+        val icon = (intent?.getIntExtra("app_icon", -1) ?: -1)
+            .let { if (it != -1) defaultNotificationAppearance.icon else Icon.createWithResource(context, it) }
+
+        notificationOpenActivityIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            intent?.getParcelableExtra("open_activity_intent", Intent::class.java)
+        } else {
+            intent?.getParcelableExtra<Intent>("open_activity_intent")
+        }
+
+        return NotificationAppearance(
+            name = name,
+            titleIdle = titleIdle,
+            titleWorking = titleWorking,
+            content = content,
+            stopActionText = stopActionText,
+            icon = icon,
+        )
+    }
+
+    /**
+     * create notification with initial state idle.
+     */
+    fun createNotification(service: Service) {
+        val currentNotification = notificationService.activeNotifications.find { it.id == NOTIFICATION_ID }
+        if (currentNotification != null) return
+
+        val notification = buildNotification(
+            notificationAppearance,
+            NotificationDisplayStrategy.Idle(0.bytes, 0.bytes),
+        )
+        service.startForeground(NOTIFICATION_ID, notification)
+    }
+
+    /**
+     * update notification to current state.
+     */
+    fun updateNotification(displayStrategy: NotificationDisplayStrategy) {
+        val currentNotification = notificationService.activeNotifications.find { it.id == NOTIFICATION_ID }
+        if (currentNotification == null) return
+
+        val notification = buildNotification(notificationAppearance, displayStrategy)
+        notificationService.notify(NOTIFICATION_ID, notification)
+    }
+
+    /**
+     * build notification
+     */
+    private fun buildNotification(
+        appearance: NotificationAppearance,
+        displayStrategy: NotificationDisplayStrategy
+    ): Notification {
+        val openActivityAction = if (notificationOpenActivityIntent == null) null else
+            PendingIntent.getActivity(
+                context,
+                0,
+                notificationOpenActivityIntent,
+                PendingIntent.FLAG_IMMUTABLE,
+            )
+
+        return Notification.Builder(context, notificationChannel.id).apply {
+            setContentTitle(
+                when (displayStrategy) {
+                    is NotificationDisplayStrategy.Idle -> appearance.titleIdle
+                    is NotificationDisplayStrategy.Working ->
+                        String.format(appearance.titleWorking, displayStrategy.taskCount)
+                },
+            )
+            setContentText(
+                String.format(
+                    notificationAppearance.content,
+                    "${displayStrategy.downloadSpeed}/s",
+                    "${displayStrategy.uploadSpeed}/s",
+                ),
+            )
+            setSmallIcon(notificationAppearance.icon)
+            setContentIntent(openActivityAction)
+            setActions(
+                Notification.Action.Builder(
+                    notificationAppearance.icon,
+                    notificationAppearance.stopActionText,
+                    stopServiceIntent,
+                ).build(),
+            )
+            setTicker(notificationAppearance.name)
+            setOngoing(displayStrategy is NotificationDisplayStrategy.Working)
+            setVisibility(Notification.VISIBILITY_PUBLIC)
+        }.build()
+    }
+
+    companion object {
+        private const val NOTIFICATION_ID = 114
+        private const val NOTIFICATION_CHANNEL_ID = "me.him188.ani.app.domain.torrent.service.AniTorrentService"
+
+        private val defaultNotificationAppearance = NotificationAppearance(
+            name = "Animeko BT 引擎服务",
+            titleIdle = "没有下载任务",
+            titleWorking = "正在下载 %1\$d 个资源",
+            content = "下载：%1\$s，上传：%2\$s",
+            stopActionText = "停止",
+            icon = Icon.createWithBitmap(Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)),
+        )
+    }
+}
+
+class NotificationAppearance(
+    val name: String,
+    val titleIdle: String,
+    val titleWorking: String,
+    val content: String,
+    val stopActionText: String,
+    val icon: Icon,
+)
+
+sealed class NotificationDisplayStrategy(val downloadSpeed: FileSize, val uploadSpeed: FileSize) {
+    class Idle(
+        downloadSpeed: FileSize,
+        uploadSpeed: FileSize
+    ) : NotificationDisplayStrategy(downloadSpeed, uploadSpeed)
+
+    class Working(
+        downloadSpeed: FileSize,
+        uploadSpeed: FileSize,
+        val taskCount: Int,
+    ) : NotificationDisplayStrategy(downloadSpeed, uploadSpeed)
+}
