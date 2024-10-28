c2953d4a1bfe27deaa5bc664f4f930aad15a7d91
StageGuard <sg@mamoe.net>
2024-10-28 20:26:28 +0800

    jvm field

--- torrent/api/src/commonMain/kotlin/api/pieces/PieceList.kt
+++ torrent/api/src/commonMain/kotlin/api/pieces/PieceList.kt
@@ -19,6 +19,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.suspendCancellableCoroutine
 import me.him188.ani.app.torrent.api.pieces.PieceListSubscriptions.Subscription
+import kotlin.jvm.JvmField
 
 /**
  * 高性能 [Piece] 集合. 每个 [PieceList] 一定包含连续的 [Piece.pieceIndex]. 可能为空.
