c5a5944b49507d5532d63b2f2eb066f426c4735a
WoLeo-Z <me@w0l.top>
2025-01-08 15:05:29 +0800

    fix

--- app/shared/ui-foundation/src/androidMain/kotlin/ui/foundation/theme/AppTheme.android.kt
+++ app/shared/ui-foundation/src/androidMain/kotlin/ui/foundation/theme/AppTheme.android.kt
@@ -10,17 +10,12 @@
 package me.him188.ani.app.ui.foundation.theme
 
 import android.os.Build
-import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import me.him188.ani.app.platform.findActivity
 
 @Composable
 actual fun AniTheme(
@@ -29,31 +24,35 @@ actual fun AniTheme(
     useDynamicTheme: Boolean,
     content: @Composable () -> Unit
 ) {
-    // Set statusBarStyle & navigationBarStyle
-    val activity = LocalContext.current.findActivity() as? ComponentActivity
-    if (activity != null) {
-        DisposableEffect(activity, darkTheme) {
-            if (darkTheme) {
-                activity.enableEdgeToEdge(
-                    statusBarStyle = SystemBarStyle.dark(android.graphics.Color.TRANSPARENT),
-                    navigationBarStyle = SystemBarStyle.dark(android.graphics.Color.TRANSPARENT),
-                )
-            } else {
-                activity.enableEdgeToEdge(
-                    statusBarStyle = SystemBarStyle.light(
-                        android.graphics.Color.TRANSPARENT,
-                        android.graphics.Color.TRANSPARENT,
-                    ),
-                    navigationBarStyle = SystemBarStyle.light(
-                        android.graphics.Color.TRANSPARENT,
-                        android.graphics.Color.TRANSPARENT,
-                    ),
-                )
-            }
-            onDispose { }
-        }
-    }
+    // 切换 useDynamicTheme 改变颜色方案，会触发 Activity 重建
+    // app\android\src\main\kotlin\activity\MainActivity.kt
+    // TODO: ?
 
+//    // Set statusBarStyle & navigationBarStyle
+//    val activity = LocalContext.current.findActivity() as? ComponentActivity
+//    if (activity != null) {
+//        DisposableEffect(activity, darkTheme) {
+//            if (darkTheme) {
+//                activity.enableEdgeToEdge(
+//                    statusBarStyle = SystemBarStyle.dark(android.graphics.Color.TRANSPARENT),
+//                    navigationBarStyle = SystemBarStyle.dark(android.graphics.Color.TRANSPARENT),
+//                )
+//            } else {
+//                activity.enableEdgeToEdge(
+//                    statusBarStyle = SystemBarStyle.light(
+//                        android.graphics.Color.TRANSPARENT,
+//                        android.graphics.Color.TRANSPARENT,
+//                    ),
+//                    navigationBarStyle = SystemBarStyle.light(
+//                        android.graphics.Color.TRANSPARENT,
+//                        android.graphics.Color.TRANSPARENT,
+//                    ),
+//                )
+//            }
+//            onDispose { }
+//        }
+//    }
+    
     // Use Dynamic Theme
     if (useDynamicTheme) {
         if (Build.VERSION.SDK_INT >= 31) {
