d55ab7477b32b549f2febc96abb53d4c1e58fab8
WoLeo-Z <me@w0l.top>
2025-01-12 20:28:14 +0800

    fix CarouselItemColors

--- app/shared/ui-foundation/src/commonMain/kotlin/ui/foundation/layout/CarouselItem.kt
+++ app/shared/ui-foundation/src/commonMain/kotlin/ui/foundation/layout/CarouselItem.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 OpenAni and contributors.
+ * Copyright (C) 2024-2025 OpenAni and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
@@ -133,7 +133,10 @@ object CarouselItemDefaults {
 
     @Composable
     fun colors(): CarouselItemColors = appColorScheme(isDark = true).run {
-        return CarouselItemColors(MaterialTheme.colorScheme.onSurface, MaterialTheme.colorScheme.onSurface)
+        CarouselItemColors(
+            titleColor = onSurface,
+            supportingTextColor = onSurface,
+        )
     }
 
     @Composable
