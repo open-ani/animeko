3b4ba11926ad4981871f156d24359ee30a32db8c
vvbbnn00 <vvbbnn00@foxmail.com>
2025-01-22 06:33:58 +0800

    Optimize import

--- app/shared/app-data/src/commonMain/kotlin/data/repository/episode/EpisodeCollectionRepository.kt
+++ app/shared/app-data/src/commonMain/kotlin/data/repository/episode/EpisodeCollectionRepository.kt
@@ -21,9 +21,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.toList
