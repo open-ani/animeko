987aeca3599f4d5f72e6856879c8f1b0d5ec9368
Him188 <Him188@mamoe.net>
2025-01-21 17:14:54 +0000

    Migrate chips and buttons according to border outline -> outlineVariant color change.
    Affected:
    - MediaSelectorFilterChip
    - SubjectCollectionTypeButton
    - MediaSourceResultCard

--- app/shared/src/commonMain/kotlin/ui/subject/episode/mediaFetch/MediaSourceResultsView.kt
+++ app/shared/src/commonMain/kotlin/ui/subject/episode/mediaFetch/MediaSourceResultsView.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 OpenAni and contributors.
+ * Copyright (C) 2024-2025 OpenAni and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
@@ -36,6 +36,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.InputChip
+import androidx.compose.material3.InputChipDefaults
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
@@ -362,6 +363,11 @@ private fun MediaSourceResultCard(
                     source.info,
                 )
             },
+            border = InputChipDefaults.inputChipBorder(
+                enabled = true,
+                selected = selected,
+                borderColor = MaterialTheme.colorScheme.outline,
+            ),
         )
     }
 }
