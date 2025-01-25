a5b1af4f14169134d63532374e2f9bf452f30b99
WoLeo-Z <me@w0l.top>
2025-01-25 19:28:23 +0800

    Fix asComposeImageBitmap

--- app/shared/ui-subject/src/androidMain/kotlin/ui/subject/details/SubjectDetailsPage.android.kt
+++ app/shared/ui-subject/src/androidMain/kotlin/ui/subject/details/SubjectDetailsPage.android.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 OpenAni and contributors.
+ * Copyright (C) 2024-2025 OpenAni and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
@@ -12,7 +12,11 @@ package me.him188.ani.app.ui.subject.details
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.tooling.preview.Preview
+import coil3.Image
+import coil3.toBitmap
 import me.him188.ani.app.domain.foundation.LoadError
 import me.him188.ani.app.ui.foundation.ProvideFoundationCompositionLocalsForPreview
 import me.him188.ani.app.ui.foundation.widgets.BackNavigationIconButton
@@ -20,13 +24,18 @@ import me.him188.ani.app.ui.subject.details.state.SubjectDetailsStateLoader
 import me.him188.ani.app.ui.subject.details.state.createTestSubjectDetailsState
 import me.him188.ani.utils.platform.annotations.TestOnly
 
+actual fun Image.toComposeImageBitmap(): ImageBitmap {
+    return this.toBitmap().asImageBitmap()
+}
+
 @OptIn(TestOnly::class)
 @Preview
 @Preview(device = "spec:width=1280dp,height=800dp,dpi=240")
 @Composable
 internal fun PreviewSubjectDetails() = ProvideFoundationCompositionLocalsForPreview {
     val scope = rememberCoroutineScope()
-    val state = remember { SubjectDetailsStateLoader.LoadState.Ok(createTestSubjectDetailsState(scope)) }
+    val state =
+        remember { SubjectDetailsStateLoader.LoadState.Ok(createTestSubjectDetailsState(scope)) }
     SubjectDetailsPage(
         state,
         onPlay = { },
@@ -42,7 +51,12 @@ internal fun PreviewSubjectDetails() = ProvideFoundationCompositionLocalsForPrev
 internal fun PreviewPlaceholderSubjectDetails() = ProvideFoundationCompositionLocalsForPreview {
     val scope = rememberCoroutineScope()
     val state = remember {
-        SubjectDetailsStateLoader.LoadState.Ok(createTestSubjectDetailsState(scope, isPlaceholder = true))
+        SubjectDetailsStateLoader.LoadState.Ok(
+            createTestSubjectDetailsState(
+                scope,
+                isPlaceholder = true
+            )
+        )
     }
     SubjectDetailsPage(
         state,
@@ -59,7 +73,11 @@ internal fun PreviewPlaceholderSubjectDetails() = ProvideFoundationCompositionLo
 @Composable
 internal fun PreviewErrorSubjectDetails() = ProvideFoundationCompositionLocalsForPreview {
     val state = remember {
-        SubjectDetailsStateLoader.LoadState.Err(TestSubjectInfo.subjectId, TestSubjectInfo, LoadError.NetworkError)
+        SubjectDetailsStateLoader.LoadState.Err(
+            TestSubjectInfo.subjectId,
+            TestSubjectInfo,
+            LoadError.NetworkError
+        )
     }
     SubjectDetailsPage(
         state,
