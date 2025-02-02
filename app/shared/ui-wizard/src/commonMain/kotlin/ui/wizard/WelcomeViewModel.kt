a27edcd187ed991e0f389f5cb564d5ea980b3ee3
StageGuard <sg@mamoe.net>
2025-02-02 19:50:25 +0800

    todo

--- app/shared/ui-wizard/src/commonMain/kotlin/ui/wizard/WelcomeViewModel.kt
+++ app/shared/ui-wizard/src/commonMain/kotlin/ui/wizard/WelcomeViewModel.kt
@@ -98,8 +98,9 @@ import kotlin.time.Duration.Companion.days
 import kotlin.time.Duration.Companion.seconds
 
 /**
- * Todo: 这些页面的 view model 逻辑实际上互不相关
- *  所以可以考虑拆分逻辑 至 use case, 与设置页面 view model 对应的逻辑合并
+ * Todo: 这些页面的 view model 逻辑实际上互不相关.
+ *  所以可以考虑拆分逻辑至 use case, 与设置页面 view model 对应的逻辑合并.
+ *  这样的好处很明显, 减少 view model 的复杂度, 使其更容易维护, 也方便写单元测试.
  */
 class WelcomeViewModel : AbstractSettingsViewModel(), KoinComponent {
     private val settingsRepository: SettingsRepository by inject()
