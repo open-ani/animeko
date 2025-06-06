42dc42ddc5e01aec5fd2fbf2861dc458d46754c4
Him188 <Him188@mamoe.net>
2025-06-06 17:00:03 +0100

    install glew

--- ci-helper/install-deps-windows.cmd
+++ ci-helper/install-deps-windows.cmd
@@ -5,6 +5,8 @@
 vcpkg install openssl:x64-windows boost-variant:x64-windows boost-system:x64-windows boost-range:x64-windows boost-crc:x64-windows boost-logic:x64-windows boost-parameter:x64-windows boost-asio:x64-windows boost-variant2:x64-windows boost-multi-index:x64-windows boost-multiprecision:x64-windows
 @REM vcpkg install openssl:x64-windows boost:x64-windows
 
+vcpkg install glew:x64-windows
+
 choco install swig -y
 choco install openssl -y
 
