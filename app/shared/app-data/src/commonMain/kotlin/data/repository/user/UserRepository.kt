cf093cfcd8f051418d8d2ab8662b4957e6b63d5f
StageGuard <sg@mamoe.net>
2025-06-18 10:32:37 +0800

    resolve bangumiUserId from server.

--- app/shared/app-data/src/commonMain/kotlin/data/repository/user/UserRepository.kt
+++ app/shared/app-data/src/commonMain/kotlin/data/repository/user/UserRepository.kt
@@ -1,3 +1,12 @@
+/*
+ * Copyright (C) 2024-2025 OpenAni and contributors.
+ *
+ * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
+ * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
+ *
+ * https://github.com/open-ani/ani/blob/main/LICENSE
+ */
+
 package me.him188.ani.app.data.repository.user
 
 import androidx.datastore.core.DataStore
@@ -145,5 +154,6 @@ private fun AniAniSelfUser.toSelfInfo(): SelfInfo {
         email = email,
         hasPassword = hasPassword,
         avatarUrl = largeAvatar,
+        bangumiUsername = bangumiUsername,
     )
 }
