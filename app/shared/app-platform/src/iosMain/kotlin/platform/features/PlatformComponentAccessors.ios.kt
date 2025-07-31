a1118fe63a47d9e2ebe6f4ba2efb310a4571be51
KAMOsama <99065946+KAMO030@users.noreply.github.com>
2025-07-31 15:34:36 +0800

    feat(ios): 将视频音频视为媒体音频，以便于支持在静音模式下正常播放
    - 实现 `IosAudioManager`，支持获取和设置系统音量
    - 优化 `MPVolumeView` 的显示和移除逻辑，防止视觉干扰
    - 配置 `AVAudioSession`，切换到播放类别并激活

--- app/shared/app-platform/src/iosMain/kotlin/platform/features/PlatformComponentAccessors.ios.kt
+++ app/shared/app-platform/src/iosMain/kotlin/platform/features/PlatformComponentAccessors.ios.kt
@@ -9,14 +9,29 @@
 
 package me.him188.ani.app.platform.features
 
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import me.him188.ani.app.platform.Context
+import platform.AVFAudio.AVAudioSession
+import platform.AVFAudio.outputVolume
+import platform.CoreGraphics.CGRectMake
+import platform.MediaPlayer.MPVolumeView
+import platform.UIKit.UIApplication
 import platform.UIKit.UIScreen
+import platform.UIKit.UISlider
+import platform.darwin.dispatch_async
+import platform.darwin.dispatch_get_main_queue
+import kotlin.concurrent.Volatile
 
 actual fun getComponentAccessorsImpl(context: Context): PlatformComponentAccessors =
     IosPlatformComponentAccessors
 
 private object IosPlatformComponentAccessors : PlatformComponentAccessors {
-    override val audioManager: AudioManager? get() = null // Ios does not allow adjusting system volume.
+    override val audioManager: AudioManager? by lazy { IosAudioManager() }
     override val brightnessManager: BrightnessManager? by lazy { IosBrightnessManager() }
 }
 
@@ -28,4 +43,45 @@ private class IosBrightnessManager : BrightnessManager {
     override fun setBrightness(level: Float) {
         UIScreen.mainScreen.brightness = level.coerceIn(0f, 1f).toDouble()
     }
+}
+
+internal class IosAudioManager : AudioManager {
+
+    @OptIn(ExperimentalForeignApi::class)
+    val volumeView = MPVolumeView(CGRectMake(-2000.0, -2000.0, 0.0, 0.0))
+
+    val coroutineScope = CoroutineScope(Dispatchers.Main)
+
+    
+    @Volatile
+    var currentJob: Job? = null
+
+    val slider by lazy {
+        // 在 MPVolumeView 中找到 UISlider 组件
+        volumeView.subviews.first { it is UISlider } as UISlider
+    }
+
+    override fun getVolume(streamType: StreamType): Float {
+        return slider.value
+    }
+
+    @OptIn(ExperimentalForeignApi::class)
+    override fun setVolume(streamType: StreamType, levelPercentage: Float) {
+        volumeView.alpha =  0.0 
+        if (volumeView.superview == null) {
+            UIApplication.sharedApplication.keyWindow?.addSubview(volumeView)
+        }
+        dispatch_async(queue = dispatch_get_main_queue()) {
+            // 设置音量
+            slider.setValue(levelPercentage.coerceIn(0f, 1f), animated = false)
+        }
+        // 防抖移除 MPVolumeView
+        currentJob?.cancel()
+        currentJob = coroutineScope.launch {
+            delay(3000)
+            // 移除 MPVolumeView
+            volumeView.removeFromSuperview()
+        }
+    }
+
 }
