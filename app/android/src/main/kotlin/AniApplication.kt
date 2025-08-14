5252c31b66d83868280a6d993c11968fe5b1d16e
Mystery406 <648353188@qq.com>
2025-08-14 09:25:06 +0800

    Revert "Update language handling logic for API level 33 compatibility."
    This reverts commit 74688d7f26ef2ac7c83e2f5ebb38cb6405d21631.

--- app/android/src/main/kotlin/AniApplication.kt
+++ app/android/src/main/kotlin/AniApplication.kt
@@ -24,10 +24,13 @@ import androidx.lifecycle.ProcessLifecycleOwner
 import io.ktor.client.engine.okhttp.OkHttp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import me.him188.ani.android.activity.MainActivity
+import me.him188.ani.app.data.persistent.MemoryDataStore
 import me.him188.ani.app.data.persistent.dataStores
 import me.him188.ani.app.data.repository.user.SettingsRepository
 import me.him188.ani.app.domain.media.cache.storage.MediaCacheSave
@@ -246,12 +249,13 @@ class AniApplication : Application() {
 
     fun Context.applyLanguage(languageTag: String) {
         val locales = LocaleListCompat.forLanguageTags(languageTag)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU /* 33 */) {
-            // API‑33+: LocaleManager is still the recommended path
+        AppCompatDelegate.setApplicationLocales(locales)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE /* 34 */) {
+            // API‑34+: LocaleManager is still the recommended path
             val localeManager = getSystemService(android.app.LocaleManager::class.java)
             localeManager.applicationLocales = locales.unwrap() as LocaleList
         } else {
-            // AndroidX AppCompat ≥ 1.6 handles API 14‑32
+            // AndroidX AppCompat ≥ 1.6 handles API 14‑33
             AppCompatDelegate.setApplicationLocales(locales)
         }
     }
