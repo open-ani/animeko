832293db44012c792547752999537593fe8668ad
KAMOsama <99065946+KAMO030@users.noreply.github.com>
2025-09-01 13:33:01 +0800

    feat(ios): 配置音频会话以支持在静音模式下播放声音
    - 设置 `AVAudioSession` 的类别为 `Playback`，支持后台音频播放
    - 激活音频会话

--- app/shared/application/src/iosMain/kotlin/ios/AniIos.kt
+++ app/shared/application/src/iosMain/kotlin/ios/AniIos.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.ComposeUIViewController
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
@@ -92,6 +93,9 @@ import org.koin.core.context.startKoin
 import org.koin.dsl.module
 import org.openani.mediamp.MediampPlayerFactory
 import org.openani.mediamp.avkit.AVKitMediampPlayerFactory
+import platform.AVFAudio.AVAudioSession
+import platform.AVFAudio.AVAudioSessionCategoryPlayback
+import platform.AVFAudio.setActive
 import platform.UIKit.NSLayoutConstraint
 import platform.UIKit.UIViewController
 import platform.UIKit.addChildViewController
@@ -271,7 +275,11 @@ fun getIosModules(
             mediaResolver = get<MediaResolver>(),
         )
     }
+    @OptIn(ExperimentalForeignApi::class)
     single<MediampPlayerFactory<*>> {
+        val audioSession = AVAudioSession.sharedInstance()
+        audioSession.setCategory(AVAudioSessionCategoryPlayback, null)
+        audioSession.setActive(true, null)
         AVKitMediampPlayerFactory()
     }
     single<MediaSaveDirProvider> {
