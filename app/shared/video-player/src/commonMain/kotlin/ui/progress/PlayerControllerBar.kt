16c42c09c07e41eb876e25a692c4e4b9309dc1a8
Him188 <Him188@mamoe.net>
2025-11-01 17:26:14 +0800

    修复浅色模式下，自动跳过 op 的提示看不到, fix #2644

--- app/shared/video-player/src/commonMain/kotlin/ui/progress/PlayerControllerBar.kt
+++ app/shared/video-player/src/commonMain/kotlin/ui/progress/PlayerControllerBar.kt
@@ -14,7 +14,6 @@ import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.hoverable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -93,10 +92,12 @@ import androidx.compose.ui.window.Popup
 import androidx.compose.ui.window.PopupProperties
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.flow.Flow
+import me.him188.ani.app.data.models.preference.DarkMode
 import me.him188.ani.app.domain.media.player.MediaCacheProgressInfo
 import me.him188.ani.app.ui.foundation.dialogs.PlatformPopupProperties
 import me.him188.ani.app.ui.foundation.effects.onKey
 import me.him188.ani.app.ui.foundation.ifThen
+import me.him188.ani.app.ui.foundation.theme.AniTheme
 import me.him188.ani.app.ui.foundation.theme.slightlyWeaken
 import me.him188.ani.app.ui.foundation.theme.stronglyWeaken
 import me.him188.ani.app.videoplayer.ui.PlaybackSpeedControllerState
@@ -579,19 +580,22 @@ object PlayerControllerDefaults {
         onClick: () -> Unit,
         modifier: Modifier = Modifier
     ) {
-        Box(
-            modifier = modifier.clip(CircleShape)
-                .background(MaterialTheme.colorScheme.surface),
-            contentAlignment = Alignment.CenterStart,
-        ) {
-            Row(verticalAlignment = Alignment.CenterVertically) {
+        AniTheme(darkModeOverride = DarkMode.DARK) {
+            Surface(
+                modifier = modifier,
+                shape = CircleShape,
+                color = MaterialTheme.colorScheme.surface,
+                contentColor = MaterialTheme.colorScheme.onSurface,
+            ) {
                 ProvideTextStyle(MaterialTheme.typography.labelLarge) {
-                    Text(
-                        text = "即将跳过 OP 或 ED",
-                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 12.dp),
-                    )
-                    TextButton(onClick = onClick) {
-                        Text("取消")
+                    Row(
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        Text("即将跳过 OP 或 ED")
+                        TextButton(onClick = onClick) {
+                            Text("取消")
+                        }
                     }
                 }
             }
