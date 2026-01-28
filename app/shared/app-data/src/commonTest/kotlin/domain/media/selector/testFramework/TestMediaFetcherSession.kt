0dbff9b370dbe3be1c4c1fd073d4ac7d02235b0e
StageGuard <sg@mamoe.net>
2026-01-28 22:30:33 +0800

    Fix test.

--- app/shared/app-data/src/commonTest/kotlin/domain/media/selector/testFramework/TestMediaFetcherSession.kt
+++ app/shared/app-data/src/commonTest/kotlin/domain/media/selector/testFramework/TestMediaFetcherSession.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024-2025 OpenAni and contributors.
+ * Copyright (C) 2024-2026 OpenAni and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
@@ -102,7 +102,7 @@ class TestMediaFetchSessionBuilder {
     ): TestMediaFetchSession<R> {
         return TestMediaFetchSession(
             MediaSourceMediaFetcher(
-                configProvider = { MediaFetcherConfig() },
+                configProvider = { MediaFetcherConfig(true) },
                 mediaSources = instances.map { it.instance },
                 flowContext = dispatcher,
             ).newSession(mediaFetchRequestBuilder.build()),
@@ -132,9 +132,9 @@ class Handle(
         result.complete(
             medias.map {
                 it.copy(
-                    (it.media as DefaultMedia).copy(mediaSourceId = instance.mediaSourceId)
+                    (it.media as DefaultMedia).copy(mediaSourceId = instance.mediaSourceId),
                 )
-            }
+            },
         )
         return this
     }
