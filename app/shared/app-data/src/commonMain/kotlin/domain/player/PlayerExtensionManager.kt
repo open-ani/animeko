7b2754dfd69b8ba854bd10976940066acbb72a44
StageGuard <sg@mamoe.net>
2026-04-11 18:15:14 +0800

    Add event broadcaster for player extension, allowing extensions to run in sequence. Adapt test case to new mediamp architecture. Only launch play progress saver and episode switcher after media is loaded.

--- app/shared/app-data/src/commonMain/kotlin/domain/player/PlayerExtensionManager.kt
+++ app/shared/app-data/src/commonMain/kotlin/domain/player/PlayerExtensionManager.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 OpenAni and contributors.
+ * Copyright (C) 2024-2026 OpenAni and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
@@ -9,7 +9,9 @@
 
 package me.him188.ani.app.domain.player
 
+import kotlinx.coroutines.channels.BufferOverflow
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import me.him188.ani.app.domain.episode.EpisodeFetchSelectPlayState
 import me.him188.ani.app.domain.episode.EpisodeSession
 import me.him188.ani.app.domain.episode.UnsafeEpisodeSessionApi
@@ -18,34 +20,17 @@ import me.him188.ani.app.domain.episode.player
 import me.him188.ani.app.domain.player.extension.EpisodePlayerExtensionFactory
 import me.him188.ani.app.domain.player.extension.PlayerExtension
 import me.him188.ani.app.domain.player.extension.PlayerExtensionContext
+import me.him188.ani.app.domain.player.extension.PlayerExtensionEvent
 import org.koin.core.Koin
 import org.openani.mediamp.MediampPlayer
 import kotlin.coroutines.cancellation.CancellationException
 
 class PlayerExtensionManager(
-    val extensions: List<PlayerExtension>,
-) {
-    inline fun call(block: (PlayerExtension) -> Unit) {
-        extensions.forEach {
-            try {
-                block(it)
-            } catch (e: Throwable) {
-                if (e is CancellationException) {
-                    throw e
-                }
-
-                throw ExtensionException("Error calling extension ${it.name}, see cause", e)
-            }
-        }
-    }
-}
-
-fun PlayerExtensionManager(
-    extensions: List<EpisodePlayerExtensionFactory<*>>,
+    factories: List<EpisodePlayerExtensionFactory<*>>,
     state: EpisodeFetchSelectPlayState,
     koin: Koin,
-): PlayerExtensionManager {
-    val context = object : PlayerExtensionContext {
+) {
+    private val context = object : PlayerExtensionContext {
         override val subjectId: Int
             get() = state.subjectId
 
@@ -55,6 +40,9 @@ fun PlayerExtensionManager(
             get() = state.playerSession.videoLoadingState
         override val sessionFlow: Flow<EpisodeSession>
             get() = state.episodeSessionFlow
+
+        override val broadcastEvent: MutableSharedFlow<PlayerExtensionEvent> =
+            MutableSharedFlow(0, 1, BufferOverflow.DROP_OLDEST)
 
         @UnsafeEpisodeSessionApi
         override suspend fun getCurrentEpisodeId(): Int {
@@ -69,9 +57,29 @@ fun PlayerExtensionManager(
 
             state.switchEpisode(newEpisodeId)
         }
+
+        override suspend fun broadcast(event: PlayerExtensionEvent) {
+            broadcastEvent.emit(event)
+        }
     }
 
-    return PlayerExtensionManager(extensions.map { it.create(context, koin) })
+    val extensions: List<PlayerExtension> by lazy {
+        factories.map { it.create(context, koin) }
+    }
+
+    inline fun call(block: (PlayerExtension) -> Unit) {
+        extensions.forEach {
+            try {
+                block(it)
+            } catch (e: Throwable) {
+                if (e is CancellationException) {
+                    throw e
+                }
+
+                throw ExtensionException("Error calling extension ${it.name}, see cause", e)
+            }
+        }
+    }
 }
 
 class ExtensionException(message: String? = null, cause: Throwable? = null) : Exception(message, cause)
