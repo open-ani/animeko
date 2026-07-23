a15dfd5d9f3131828e59ba82335679f0347dbe88
StageGuard <sg@mamoe.net>
2026-07-24 01:08:26 +0800

    Disable DesktopOnnxImageCaptchaRecognizerTest on windows aarch64.

--- app/shared/app-data/src/desktopTest/kotlin/domain/mediasource/web/DesktopOnnxImageCaptchaRecognizerTest.kt
+++ app/shared/app-data/src/desktopTest/kotlin/domain/mediasource/web/DesktopOnnxImageCaptchaRecognizerTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2026 OpenAni and contributors.
+ * Copyright (C) 2024-2026 OpenAni and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license, which can be found at the following link.
@@ -11,9 +11,16 @@ package me.him188.ani.app.domain.mediasource.web
 
 import kotlinx.coroutines.test.runTest
 import me.him188.ani.app.domain.mediasource.web.captcha.ImageCaptchaSample
+import org.junit.jupiter.api.condition.DisabledOnOs
+import org.junit.jupiter.api.condition.OS
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
+@DisabledOnOs(
+    value = [OS.WINDOWS],
+    architectures = ["aarch64", "arm64", "arm"],
+    disabledReason = "Microsoft onnxruntime doesn't support windows aarch64.",
+)
 class DesktopOnnxImageCaptchaRecognizerTest {
     @Test
     fun `recognizes a labeled real captcha sample`() = runTest {
